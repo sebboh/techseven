1a93aedcaf9f41a352172a33036493cd6dc6a689
Steve Chen <sebboh@gmail.com>
2014-04-25 12:36:15 -0700

    adds basic layout, bootstrap, unicorn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = root_name
+    page_title.empty? ? base_title : "#{base_title} | #{page_title}"
+  end
+
+  def root_name
+    "TECHseven"
+  end
 end
